7d8889d4e249eaf8d0971011ea1bcaaae052bd19
WaiYanLatt <humanant71@gmail.com>
2023-01-10 19:34:00 +0630

    first commit

---
+++ assets/js/cus.js
@@ -0,0 +1,28 @@
+const toggleB = document.getElementById("toggleB");
+
+const changeThemetoDark = () => {
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("data-theme", "dark");
+};
+
+const changeThemetoLight = () => {
+  document.documentElement.classList.remove("dark");
+  localStorage.setItem("data-theme", "light");
+};
+
+toggleB.addEventListener("click", () => {
+  const theme = localStorage.getItem("data-theme");
+  if (theme === "dark") {
+    changeThemetoLight();
+  } else {
+    changeThemetoDark();
+  }
+});
+
+const theme = localStorage.getItem("data-theme");
+  if (theme === "dark") {
+    changeThemetoDark();
+    toggleB.setAttribute('checked', 'checked')
+  } else {
+    changeThemetoLight();
+  }
